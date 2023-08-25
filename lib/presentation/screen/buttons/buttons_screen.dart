49c13e5adaad475160746d43ade024e9590fdea5
Sergio Barreras González <sergiobg.isc@gmail.com>
2023-08-25 11:01:10 -0700

    pantalla de botones

--- lib/presentation/screen/buttons/buttons_screen.dart
+++ lib/presentation/screen/buttons/buttons_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
 
 class ButtonsScreen extends StatelessWidget {
   const ButtonsScreen({super.key});
@@ -9,7 +10,78 @@ class ButtonsScreen extends StatelessWidget {
       appBar: AppBar(
         title: const Text('Buttons screen'),
       ),
-      body: const Placeholder(),
+      body: _ButtonsView(),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          context.pop();
+        },
+        child: const Icon(Icons.arrow_back_ios_new_rounded),
+      ),
+    );
+  }
+}
+
+class _ButtonsView extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    const Icon icono = Icon(Icons.accessible);
+    final colors = Theme.of(context).colorScheme;
+
+    return SizedBox(
+      width: double.infinity,
+      child: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 10, vertical: 20),
+        child: Wrap(
+          spacing: 10,
+          crossAxisAlignment: WrapCrossAlignment.center,
+          alignment: WrapAlignment.center,
+          children: [
+            //section: Elevated button
+            ElevatedButton(onPressed: () {}, child: const Text('Elevated')),
+            const ElevatedButton(onPressed: null, child: Text('Elevated')),
+            ElevatedButton.icon(
+                onPressed: () {}, icon: icono, label: const Text('Elevated')),
+            ElevatedButton.icon(
+                onPressed: null, icon: icono, label: const Text('Elevated')),
+            //section: Filled button
+            FilledButton(onPressed: () {}, child: const Text('Filled')),
+            FilledButton.tonal(
+                onPressed: () {}, child: const Text('Filled tonal')),
+            FilledButton.icon(
+                onPressed: () {},
+                icon: icono,
+                label: const Text('Filled icon')),
+            FilledButton.tonalIcon(
+                onPressed: () {},
+                icon: icono,
+                label: const Text('Filled icon')),
+            const FilledButton(onPressed: null, child: Text('Filled')),
+            //section: Outlined button
+            OutlinedButton(onPressed: () {}, child: const Text('Outlined')),
+            OutlinedButton.icon(
+                onPressed: () {}, icon: icono, label: const Text('Outlined')),
+            const OutlinedButton(onPressed: null, child: Text('Outlined ')),
+            //section: Text button
+            TextButton(onPressed: () {}, child: const Text('Text')),
+            TextButton.icon(
+                onPressed: () {}, icon: icono, label: const Text('Text')),
+            const TextButton(onPressed: null, child: Text('Text')),
+            //section: Icon buttons
+            IconButton(
+              onPressed: () {},
+              icon: icono,
+            ),
+            IconButton(
+              onPressed: () {},
+              icon: icono,
+              style: ButtonStyle(
+                  backgroundColor: MaterialStatePropertyAll(colors.primary),
+                  iconColor: const MaterialStatePropertyAll(Colors.white)),
+            ),
+            //section: Custom buttons
+          ],
+        ),
+      ),
     );
   }
 }
