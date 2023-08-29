382f0e96637c212d460954c9adc6711049c64e24
Sergio Barreras González <sergiobg.isc@gmail.com>
2023-08-29 09:09:05 -0700

    progress indicators finished

--- lib/config/menu/menu_items.dart
+++ lib/config/menu/menu_items.dart
@@ -24,4 +24,9 @@ const appMenuItems = <MenuItem>[
       subtitle: 'Un contendor estilizado',
       link: '/cards',
       icon: Icons.credit_card),
+  MenuItem(
+      title: 'Indicador de progreso',
+      subtitle: 'Generales y controlados',
+      link: '/progress',
+      icon: Icons.refresh_rounded)
 ];
