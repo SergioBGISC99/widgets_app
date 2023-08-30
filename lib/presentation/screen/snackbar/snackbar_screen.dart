10fb5725de3c86691b609fb4a19465bb826e8e75
Sergio Barreras González <sergiobg.isc@gmail.com>
2023-08-30 09:07:54 -0700

    diálogos terminados

--- lib/presentation/screen/snackbar/snackbar_screen.dart
+++ lib/presentation/screen/snackbar/snackbar_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
 
 class SnackbarScreen extends StatelessWidget {
   static const String name = "snackbar_screen";
@@ -20,11 +21,48 @@ class SnackbarScreen extends StatelessWidget {
     ScaffoldMessenger.of(context).showSnackBar(snackbar);
   }
 
+  void openDialog(BuildContext context) {
+    showDialog(
+        context: context,
+        barrierDismissible: false,
+        builder: (context) => AlertDialog(
+              title: const Text('¿Estás seguro'),
+              content: const Text(
+                  'Magna laborum ipsum ipsum ex sunt labore sunt. Sunt cillum enim enim sunt id excepteur ad eiusmod consectetur veniam mollit enim deserunt proident. Magna sunt cillum fugiat laboris tempor. Reprehenderit nisi elit non nulla.'),
+              actions: [
+                TextButton(
+                    onPressed: () => context.pop(),
+                    child: const Text('Cancelar')),
+                FilledButton(
+                    onPressed: () => context.pop(),
+                    child: const Text('Aceptar'))
+              ],
+            ));
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: const Text('Snackbar Screen'),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            FilledButton.tonal(
+                onPressed: () {
+                  showAboutDialog(context: context, children: [
+                    const Text(
+                        'Laborum labore dolor et nostrud tempor in esse dolore. Ea id culpa ullamco do excepteur culpa eu in culpa labore esse ea. Dolore reprehenderit excepteur commodo nostrud fugiat esse cillum nulla deserunt duis incididunt cupidatat adipisicing proident.')
+                  ]);
+                },
+                child: const Text('Licencias usadas')),
+            FilledButton.tonal(
+                onPressed: () => openDialog(context),
+                child: const Text('Mostrar diálogo'))
+          ],
+        ),
       ),
       floatingActionButton: FloatingActionButton.extended(
           onPressed: () => showCustomSnackbar(context),
